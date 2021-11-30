11f06de478a1464d5f31a453fcf8a6823e5bba66
andrewlee977 <andrew.lee97@gmail.com>
2021-11-30 14:54:25 -0800

    connect to web3 API, display user's ETH balance

--- pages/index.py
+++ pages/index.py
@@ -1,13 +1,22 @@
 # Imports from 3rd party libraries
+from os import getenv
 import dash
 import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from dash.exceptions import PreventUpdate
 import plotly.express as px
+
+from web3 import Web3
 
 # Imports from this application
 from app import app
+
+infura_url = getenv("INFURA_ENDPOINT")
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+
 
 # 2 column layout. 1st column width = 4/12
 # https://dash-bootstrap-components.opensource.faculty.ai/l/components/layout
@@ -22,10 +31,16 @@ column1 = dbc.Col(
 
             """
         ),
-        dcc.Input(id='search'),
+        dcc.Input(id='address', placeholder="Ethereum Wallet Address", type="text", style={'width': '80%'}),
         html.Br(),
         html.Br(),
-        dcc.Link(dbc.Button('Search Address', color='primary'), href='/dashboard')
+        dbc.Button('Search Address', id='search', n_clicks=0, color='primary'),
+
+
+        html.Br(),
+        html.Br(),
+        dcc.Markdown("# BALANCE"),
+        html.Div(id='balance'),
     ],
     md=4,
 )
@@ -37,8 +52,28 @@ gapminder = px.data.gapminder()
 column2 = dbc.Col(
     [
         # dcc.Graph(figure=fig),
-        html.Img(src="assets/crypto-dashboard.jpeg", width='725px', height='400px')
+        # html.Img(src="assets/crypto-dashboard.jpeg", width='725px', height='400px')
     ]
 )
 
 layout = dbc.Row([column1, column2])
+
+
+
+@app.callback(
+    Output(component_id='balance', component_property='children'),
+    [Input(component_id='search', component_property='n_clicks'),
+    Input(component_id='address', component_property='value')]
+)
+
+def get_balance(n_clicks, address):
+    if n_clicks == 0:
+        raise PreventUpdate
+    else:
+        balance_wei = web3.eth.getBalance(address)
+        balance_eth = web3.fromWei(balance_wei, 'ether')
+        n_clicks = 0
+
+    return str(balance_eth) + ' ETH'
+
+
