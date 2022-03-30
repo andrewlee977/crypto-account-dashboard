70ad1ccbb97d5a85bf071650a29b06a4f5ef1efb
andrewlee977 <andrew.lee97@gmail.com>
2022-03-30 08:36:59 -0700

    delete unnecessary imports

--- pages/index.py
+++ pages/index.py
@@ -1,12 +1,9 @@
 # Imports from 3rd party libraries
 from os import getenv
-# from numpy import datetime_as_string
 import requests
 import dash
 from dash import html, dcc
 import dash_bootstrap_components as dbc
-# import dash_core_components as dcc
-# import dash_html_components as html
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 import plotly.express as px
